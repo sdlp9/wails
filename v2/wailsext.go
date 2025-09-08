89f8a577ce2cb0ccc375228c1aa702269b74e71f
孙登 <sundeng@apexsoft.com.cn>
2025-09-08 11:11:48 +0800

    add GetScreen GetDpi

---
+++ v2/wailsext.go
@@ -0,0 +1,30 @@
+// Package wails is the main package of the Wails project.
+// It is used by client applications.
+package wails
+
+import (
+	_ "github.com/wailsapp/wails/v2/internal/goversion" // Add Compile-Time version check for minimum go version
+	"github.com/wailsapp/wails/v2/internal/frontend/desktop/windows/winc/w32"
+	"unsafe"
+)
+
+// 获取缩放比率
+func GetDpi() (uint) {
+	var rgrc w32.RECT
+	monitor := w32.MonitorFromRect(&rgrc, w32.MONITOR_DEFAULTTONULL)
+	var monitorInfo w32.MONITORINFO
+	monitorInfo.CbSize = uint32(unsafe.Sizeof(monitorInfo))
+	if monitor != 0 && w32.GetMonitorInfo(monitor, &monitorInfo) {
+		var dpiX, dpiY uint
+		w32.GetDPIForMonitor(monitor, w32.MDT_EFFECTIVE_DPI, &dpiX, &dpiY)
+		return dpiX
+	}
+	return 0 
+}
+//获取屏幕大小
+func GetScreen() (int, int) {
+	// SM_CXFRAME: 窗口左右边框宽度
+	framex := w32.GetSystemMetrics(w32.SM_CXSCREEN)
+	framey := w32.GetSystemMetrics(w32.SM_CYSCREEN)
+	return framex, framey
+}
